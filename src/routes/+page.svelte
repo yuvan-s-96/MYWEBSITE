db0ee911413288e6f1eedb13017404ebc4014798
S.Yuvan Velkumar <111728083+yuvan-s-96@users.noreply.github.com>
2023-03-17 18:45:48 +0530

    Added basic details of the user

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- svelte-ignore a11y-img-redundant-alt -->
+<img src="http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQ1oS-DeKDIgvicoSyoD8KKoIAinTTDeC6VO7erBHEsAggFjaZYZ6YP1HkFahtlKTb_" alt="Image of paris">
+<h1>Yuvan Velkumar</h1>
+<p>College Life is one of the most remarkable and lovable times of an individual's life. Unlike School Life, College Life has a different experience, and a person needs to have this experience in his/her life. College Life exposes us to whole new experiences which we always dream of experiencing after our school life. Lucky are those who get the chance to enjoy their college life, as many people don't get this chance due to their circumstances or financial issues. For every person, College Life has a different meaning. While some people spend their college life partying with friends, others become more cautious about their careers and study hard. Whatever the way, every individual enjoys their college life and always wishes to relive that time once it is over.</p>
